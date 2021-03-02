039a3e1bc97a13bb7c1caa8728c1799a31a5bca9
Kristian Hellang <kristian@hellang.com>
2021-03-02 23:29:48 +0100

    Split benchmark into separate class

---
+++ perf/Redskap.Benchmarks/IsValidBenchmark.cs
@@ -0,0 +1,43 @@
+using BenchmarkDotNet.Attributes;
+using NinEngine;
+using NoCommons.Person;
+using Nonin;
+using TeddValidator = Tedd.Fodselsnummer.FodselsnummerValidator;
+
+namespace Redskap.Benchmarks
+{
+    public class IsValidBenchmark
+    {
+        private const string Value = "28089647063";
+
+        [Benchmark(Baseline = true)]
+        public bool Redskap()
+        {
+            return IdentificationNumber.IsValid(Value);
+        }
+
+        [Benchmark]
+        public bool Nonin()
+        {
+            return Nin.IsValid(Value);
+        }
+
+        [Benchmark]
+        public bool NoCommons()
+        {
+            return FodselsnummerValidator.IsValid(Value);
+        }
+
+        [Benchmark]
+        public bool Tedd()
+        {
+            return TeddValidator.Validate(Value).Success;
+        }
+
+        [Benchmark]
+        public bool NinEngine()
+        {
+            return NorwegianIdentity.IsValidFoedselsnummer(Value);
+        }
+    }
+}
--- perf/Redskap.Benchmarks/Program.cs
+++ perf/Redskap.Benchmarks/Program.cs
@@ -1,55 +1,19 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Configs;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Running;
-using Nonin;
-using NinEngine;
-using TeddValidator = Tedd.Fodselsnummer.FodselsnummerValidator;
-using NoCommonsValidator = NoCommons.Person.FodselsnummerValidator;
 
 namespace Redskap.Benchmarks
 {
-    [MemoryDiagnoser]
     public class Program
     {
-        private const string Value = "28089647063";
-
         public static void Main(string[] args)
         {
             var config = DefaultConfig.Instance
+                .AddDiagnoser(MemoryDiagnoser.Default)
                 // NoCommons and NinEngine ships with unoptimized assemblies
                 .WithOptions(ConfigOptions.DisableOptimizationsValidator);
 
-            BenchmarkRunner.Run<Program>(config);
-        }
-
-        [Benchmark(Baseline = true)]
-        public bool Lothbrok()
-        {
-            return IdentificationNumber.TryParse(Value, out _);
-        }
-
-        [Benchmark]
-        public bool Nonin()
-        {
-            return Nin.IsValid(Value);
-        }
-
-        [Benchmark]
-        public bool NoCommons()
-        {
-            return NoCommonsValidator.IsValid(Value);
-        }
-
-        [Benchmark]
-        public bool Tedd()
-        {
-            return TeddValidator.Validate(Value).Success;
-        }
-
-        [Benchmark]
-        public bool NinEngine()
-        {
-            return NorwegianIdentity.IsValidFoedselsnummer(Value);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
         }
     }
 }
