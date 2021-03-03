15a9a80726db1c41fa865bf4e13ae1a3aacbde49
Kristian Hellang <kristian@hellang.com>
2021-03-03 01:26:04 +0100

    Add stub for formatting

---
+++ src/Redskap/IdentificationNumber.Formatting.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Redskap
+{
+    public readonly partial struct IdentificationNumber
+    {
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
