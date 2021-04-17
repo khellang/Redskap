6470dfaa1231e388c08eefee0db62d89e8f917b2
Kristian Hellang <kristian@hellang.com>
2021-04-17 16:43:05 +0200

    Add some equality, deconstruction and formatting overrides to PostCodeInfo

--- src/Redskap/PostCodeInfo.cs
+++ src/Redskap/PostCodeInfo.cs
@@ -1,9 +1,11 @@
+using System;
+
 namespace Redskap
 {
     /// <summary>
     /// Represents informatin about a Norwegian post code from Bring's post code register.
     /// </summary>
-    public readonly struct PostCodeInfo
+    public readonly struct PostCodeInfo : IEquatable<PostCodeInfo>
     {
         /// <summary>
         /// Creates a new instance of a <see cref="PostCodeInfo"/>.
@@ -25,5 +27,42 @@ namespace Redskap
         /// The post code's postal name, i.e. OSLO.
         /// </summary>
         public string PostalName { get; }
+
+        /// <summary>
+        /// Deconstructs the post code info into the <paramref name="postCode"/>
+        /// and <paramref name="postalName"/> components.
+        /// </summary>
+        /// <param name="postCode">The post code.</param>
+        /// <param name="postalName">The post code's postal name.</param>
+        public void Deconstruct(out string postCode, out string postalName) =>
+            (postCode, postalName) = (PostCode, PostalName);
+
+        /// <inheritdoc />
+        public bool Equals(PostCodeInfo other) => PostCode == other.PostCode;
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj) => obj is PostCodeInfo other && Equals(other);
+
+        /// <inheritdoc />
+        public override int GetHashCode() => PostCode.GetHashCode();
+
+        /// <summary>
+        /// Compares two instances of <see cref="PostCodeInfo"/> for equality.
+        /// </summary>
+        /// <param name="left">The left operand.</param>
+        /// <param name="right">The right operand.</param>
+        /// <returns><see langword="true"/> if the post codes are equal; otherwise, <see langword="false"/>.</returns>
+        public static bool operator ==(PostCodeInfo left, PostCodeInfo right) => left.Equals(right);
+
+        /// <summary>
+        /// Compares two instances of <see cref="PostCodeInfo"/> for inequality.
+        /// </summary>
+        /// <param name="left">The left operand.</param>
+        /// <param name="right">The right operand.</param>
+        /// <returns><see langword="true"/> if the post codes are not equal; otherwise, <see langword="false"/>.</returns>
+        public static bool operator !=(PostCodeInfo left, PostCodeInfo right) => !left.Equals(right);
+
+        /// <inheritdoc />
+        public override string ToString() => $"{PostCode} {PostalName}";
     }
 }
