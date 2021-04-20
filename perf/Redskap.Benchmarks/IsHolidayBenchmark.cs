21d9fd5659a7214757af29acaf894ffaeaa3d55f
Kristian Hellang <kristian@hellang.com>
2021-04-20 21:00:28 +0200

    Add IsHoliday benchmarks

---
+++ perf/Redskap.Benchmarks/IsHolidayBenchmark.cs
@@ -0,0 +1,30 @@
+using System;
+using BenchmarkDotNet.Attributes;
+using Nager.Date;
+using NoCommons.Date;
+
+namespace Redskap.Benchmarks
+{
+    public class IsHolidayBenchmark
+    {
+        private static readonly DateTime Date = new(2021, 04, 01);
+
+        [Benchmark(Baseline = true)]
+        public bool Redskap()
+        {
+            return Date.IsHoliday();
+        }
+
+        [Benchmark]
+        public bool NoCommons()
+        {
+            return NorwegianDateUtil.IsHoliday(Date);
+        }
+
+        [Benchmark]
+        public bool Nager()
+        {
+            return DateSystem.IsPublicHoliday(Date, CountryCode.NO);
+        }
+    }
+}
