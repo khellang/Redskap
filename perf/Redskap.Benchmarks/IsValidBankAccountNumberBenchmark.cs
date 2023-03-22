bac8cd54b51482e4cb05f661a1dc8d45911199b3
Kristian Hellang <kristian@hellang.com>
2023-03-22 14:21:50 +0100

    Add benchmark for bank account number validation

---
+++ perf/Redskap.Benchmarks/IsValidBankAccountNumberBenchmark.cs
@@ -0,0 +1,23 @@
+using BenchmarkDotNet.Attributes;
+using NoCommons.Banking;
+using TeddValidator = Tedd.Fodselsnummer.FodselsnummerValidator;
+
+namespace Redskap.Benchmarks
+{
+    public class IsValidBankAccountNumberBenchmark
+    {
+        private const string Value = "93398290133";
+
+        [Benchmark(Baseline = true)]
+        public bool Redskap()
+        {
+            return BankAccountNumber.IsValid(Value);
+        }
+
+        [Benchmark]
+        public bool NoCommons()
+        {
+            return KontonummerValidator.IsValid(Value);
+        }
+    }
+}
